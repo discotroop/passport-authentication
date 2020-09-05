8ffd55d36e702335b61e62dd65677627d9d27d5f
John Mark Goodson <john.mark.hat@gmail.com>
2020-09-05 16:02:42 -0400

    added simplex.css from bootswatch to /public/stylesheets

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 require("dotenv").config();
 // MONGO: mongodb+srv://passenger:<password>@cluster0.bw2gg.mongodb.net/<dbname>?retryWrites=true&w=majority
+// grab a theme https://bootswatch.com/
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
-var app = express();
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
