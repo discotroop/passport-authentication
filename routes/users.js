7e9fed26c192ef90fa55d05194f7f96e4eb01444
John Mark Goodson <john.mark.hat@gmail.com>
2020-09-09 14:07:12 -0400

    added validation and error message handling to register.js

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,35 @@ router.get("/register", (req, res, next) => res.render("register"));
 
 // Register Handle
 router.post("/register", (req, res, next) => {
-  console.log(req.body);
+  const { name, email, password, passwordConfirm } = req.body;
+  let errors = [];
+
+  // check required fields / validations
+  if (!name || !email || !password || !passwordConfirm) {
+    errors.push({ message: "Please fill in all fields" });
+  }
+  // check passwords match
+  if (password != passwordConfirm) {
+    errors.push({ message: "Passwords do not match" });
+  }
+  // check minimum password length
+  if (password.length < 6) {
+    errors.push({ message: "Password must be at least 6 characters" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", {
+      errors,
+      name,
+      email,
+      password,
+      passwordConfirm
+    });
+  } else {
+    res.send("pass");
+  }
+
+  console.log(name, email, password, passwordConfirm);
   res.send("hello");
 });
 module.exports = router;
